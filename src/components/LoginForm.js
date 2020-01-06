74574c94a40ade6cd4364b4c6e9b492734e2283e
unknown <viola4lfe@gmail.com>
2020-01-06 16:53:22 -0500

    updated admin login form with initial validation

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import {
   Container,
   Col,
@@ -8,6 +8,8 @@ import {
   Input,
   Button
 } from "reactstrap";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 class loginform extends Component {
   render() {
@@ -63,4 +65,98 @@ class loginform extends Component {
 //   );
 // }
 
-export default loginform;
+// form without boot/reactstrap:
+
+// const AdminLogin = props => {
+//   const [users, setUsers] = useState({
+//     username: "",
+//     email: ""
+//   });
+
+//   const handleChanges = e => {
+//     setUsers({
+//       ...users, [e.target.name]: e.target.value
+//     })
+//     console.log(e.target.name);
+//   };
+
+//   const addNewAdmin = user => {
+//     const newAdmin ={
+//       id: Date.now(),
+//       username: user.username,
+//       email: user.email
+//     }
+//     setUsers([...users, newAdmin])
+//   }
+
+//   const submitForm = e => {
+//     e.preventDefault();
+//     addNewAdmin(users);
+//     setUsers({username: "", email: ""})
+//   };
+
+//   return (
+//     <div className="admin-login">
+//       <h4>Admin Login: </h4>
+//     <form onSubmit={submitForm} className="admin-loginForm">
+//       <label htmlFor="username">Username: </label>
+//       <input id="username" type="text" placeholder="Username" onChange={handleChanges} name="username" value={users.username}
+//       />
+//       <label htmlFor="email">Email: </label>
+//       <input id="email" type="text" placeholder="Email" onChange={handleChanges} name="email" value={users.email}
+//       />
+//       <button type="submit">Login</button>
+//     </form>
+//     </div>
+
+//   )
+// }
+
+const AdminLogin = () => {
+  const { handleSubmit, register, errors } = useForm();
+  const onSubmit = values => {
+    console.log(values);
+  };
+
+  useEffect(() => {
+    axios
+      .post("https://testing-refugee-stories.herokuapp.com/auth/login")
+      .then(response => {
+        console.log("success", response);
+      })
+      .catch(error => console.log(error.response));
+  }, []);
+
+  return (
+    <form className="admin-login" onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="email">Email: </label>
+      <input
+        name="email"
+        ref={register({
+          required: "Required",
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+            message: "invalid email address"
+          }
+        })}
+      />
+      {errors.email && errors.email.message}
+
+      <label htmlFor="password">Password: </label>
+      <input
+        name="password"
+        ref={register({
+          required: "Required",
+          message: "invalid password",
+          validate: value => value !== "admin" || "Nice try!"
+        })}
+      />
+      {errors.password && errors.password.message}
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+// export default loginform;
+export default AdminLogin;
