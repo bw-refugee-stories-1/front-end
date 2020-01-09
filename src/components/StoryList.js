34adff515f2e81cbe0e565220b25fb08b7dd942f
unknown <viola4lfe@gmail.com>
2020-01-09 11:05:12 -0500

    did a pull first, and now pushing my updates from working last night; found the extra button bug

--- src/components/StoryList.js
+++ src/components/StoryList.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 // import axios from "axios";
 import StoryCard from './StoryCard';
-import { Button } from 'reactstrap';
 import { connect } from 'react-redux';
 import { getStories } from '../actions';
 
@@ -14,7 +13,6 @@ function StoryList(props) {
 
   return (
     <div className="storyList">
-      <Button>Read More: </Button>
       <div className="entry">
         {props.storyState.stories.map(story => {
           return <StoryCard key={story.id} story={story} />;
@@ -29,5 +27,5 @@ const mapStateToProps = state => {
     storyState: state.storyState,
   };
 };
-//for the merge
+
 export default connect(mapStateToProps, { getStories })(StoryList);
