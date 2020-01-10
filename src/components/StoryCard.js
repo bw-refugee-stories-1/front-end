7609f1c67da3a3c667566556a7fdab872bea3ded
unknown <viola4lfe@gmail.com>
2020-01-10 11:50:27 -0500

    updated base scss for better story card display; need to remove some cards that are there that were prior to the random image generator

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -5,27 +5,25 @@ import { Button } from 'reactstrap';
 
 const StoryCard = props => {
   //random number generator between 0 and 4
-  const rand = Math.floor(Math.random() * 5);
-  console.log('image', props.story.id.img_url);
-  const imgArr = [
-    'https://images.unsplash.com/photo-1554428122-c2ca1f2cb88c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
-    'https://images.unsplash.com/photo-1529441435232-e28880548c25?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1551556729-c8dee4337009?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-    'https://images.unsplash.com/photo-1526817575615-3685e135615d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1288&q=80',
-    'https://images.unsplash.com/photo-1559173525-71a8abaaba32?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80',
-  ];
-  // TODO: check for the null value when getting images to see if the value actually is "null"
-  const imgSrc =
-    props.story.img_url !== null ? props.story.img_url : imgArr[rand];
+  // const rand = Math.floor(Math.random() * 5);
+  // console.log('image', props.story.id.img_url);
+  // const imgArr = [
+  //   'https://images.unsplash.com/photo-1554428122-c2ca1f2cb88c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
+  //   'https://images.unsplash.com/photo-1529441435232-e28880548c25?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+  //   'https://images.unsplash.com/photo-1551556729-c8dee4337009?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+  //   'https://images.unsplash.com/photo-1526817575615-3685e135615d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1288&q=80',
+  //   'https://images.unsplash.com/photo-1559173525-71a8abaaba32?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80',
+  // ];
+  // // TODO: check for the null value when getting images to see if the value actually is "null"
+
+  // const imgSrc =
+  //   props.story.img_url === null || props.story.img_url === ''
+  //     ? imgArr[rand]
+  //     : props.story.img_url;
 
   return (
-    <div>
-      {/* <img 
-        src="" 10-4
-        alt="Image from a refugee story"
-      /> */}
       <Card className="mt-4">
-        <CardImg top width="100%" src={imgSrc} alt="Card image cap" />
+        <CardImg top width="100%" src={props.story.img_url} alt="Card image cap" />
         <CardBody>
           <CardText className="story-text">
             {props.story.text
@@ -44,7 +42,6 @@ const StoryCard = props => {
           </Button>{' '}
         </CardBody>
       </Card>
-    </div>
   );
 };
 
